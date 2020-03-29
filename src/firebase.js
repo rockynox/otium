a42a9e50ecf78378853af3ec699332790a098cee
Léo <leo.maisonneuve-extern@renault.com>
2020-03-29 19:52:36 +0200

    ToDos tutorial

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvsWFh1ObECBcMXLhCoibc87BDSFEsZIE",
+  databaseURL: "https://otium-95ca2.firebaseio.com",
+  projectId: "otium-95ca2",
+  storageBucket: "otium-95ca2.appspot.com",
+  messagingSenderId: "692406720774",
+  appId: "1:692406720774:web:bc2b43432e0c6df958e687",
+  measurementId: "G-EVPJKBZ4QH"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const databaseRef = firebase.database().ref();
+export const todosRef = databaseRef.child("todos");
